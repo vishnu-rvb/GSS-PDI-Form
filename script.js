674c95726a64cdbf89856cbdc7b09ee45b461137
vishnu-rvb <118911871+vishnu-rvb@users.noreply.github.com>
2025-07-12 10:32:13 +0530

    minor bug fix
    refer ToDo bug points 6-8

--- script.js
+++ script.js
@@ -53,64 +53,61 @@ function clearForm() {
 
 async function submitForm(event){
     event.preventDefault();
-    disableForm(true);
-
     const URL = 'https://prod-00.centralindia.logic.azure.com:443/workflows/549c8634d35547fd816ae21d607110ab/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=z0Tw_0SxfoKUpBKZ6F-usJ1v4PuubE2QudT9ULdUmAI';
     const formData = new FormData(event.target);
-
-    // Collect PDI Inspectors
-    const Inputs_PDI_inspectors = document.querySelectorAll('#inspectors-container input[name="PDI inspector"]');
-    const data_PDI_inspectors = Array.from(Inputs_PDI_inspectors).map(input=>input.value.trim()).filter(value=>(value!==''));
-    // Collect Issues
-    const Rows_Issues = document.querySelectorAll('#issues-container .dynamic-row');
-    const data_Issues = Array.from(Rows_Issues).map( row =>{
-        const issueInput = row.querySelector('input[name="issue"]');
-        const qtyInput = row.querySelector('input[name="qty"]');
-        const statusSelect = row.querySelector('select[name="status"]');
-        return {
-            issue: issueInput?.value.trim(),
-            qty: parseInt(qtyInput?.value) || 1,
-            status: statusSelect?.value.trim()
-        };
-    });
-
-    const data = {
-        Project_Reference_number: formData.get('Project Reference number').toUpperCase(),
-        Customer_name: formData.get('Customer name'),
-        Container_number: formData.get('Container number'),
-        Container_ID: formData.get('Container ID').toUpperCase(),
-        Container_size: formData.get('Container size'),
-        Date: formData.get('Date'),
-        Shift: formData.get('Shift'),
-        PDI_inspectors: data_PDI_inspectors,
-        Issues: data_Issues,
-        Status: formData.get('Status')
-    };
-
-    //Collect images
-    const Input_PDI_report_images= document.getElementById('input-PDI report images');
-    const Inputs_PDI_loading_images= document.getElementById('input-PDI loading images');
-    const reportImgs = Array.from(Input_PDI_report_images.files);
-    const loadingImgs = Array.from(Inputs_PDI_loading_images.files);
-
-    let reportImgs_c = await compressImages(reportImgs);
-    let loadingImgs_c = await compressImages(loadingImgs);
-
-    // Create multipart/form-data payload
-    const payload = new FormData();
-    payload.append('json', new Blob([JSON.stringify(data)], { type: 'application/json' }));
-    reportImgs_c.forEach((file,index)=>
-        payload.append('PDI_report_images', file, `PDI report ${index}.${file.name.split('.').pop()}`)
-    );
-    loadingImgs_c.forEach((file,index)=>
-        payload.append('PDI_loading_images', file, `Loading image ${index}.${file.name.split('.').pop()}`)
-    );
-
-    console.log(Array.from(payload.entries()));
-
-    showLoading(true);
-
     try{
+        disableForm(true);
+        // Collect PDI Inspectors
+        const Inputs_PDI_inspectors = document.querySelectorAll('#inspectors-container input[name="PDI inspector"]');
+        const data_PDI_inspectors = Array.from(Inputs_PDI_inspectors).map(input=>input.value.trim()).filter(value=>(value!==''));
+        // Collect Issues
+        const Rows_Issues = document.querySelectorAll('#issues-container .dynamic-row');
+        const data_Issues = Array.from(Rows_Issues).map( row =>{
+            const issueInput = row.querySelector('input[name="issue"]');
+            const qtyInput = row.querySelector('input[name="qty"]');
+            const statusSelect = row.querySelector('select[name="status"]');
+            return {
+                issue: issueInput?.value.trim(),
+                qty: parseInt(qtyInput?.value) || 1,
+                status: statusSelect?.value.trim()
+            };
+        });
+
+        const data = {
+            Project_Reference_number: `${formData.get('Project Reference number')}`.toUpperCase(),
+            Customer_name: formData.get('Customer name'),
+            Container_number: formData.get('Container number'),
+            Container_ID: `${formData.get('Container ID')}`.toUpperCase(),
+            Container_size: formData.get('Container size'),
+            Date: formData.get('Date'),
+            Shift: formData.get('Shift'),
+            PDI_inspectors: data_PDI_inspectors,
+            Issues: data_Issues,
+            Status: formData.get('Status')
+        };
+
+        //Collect images
+        const Input_PDI_report_images= document.getElementById('input-PDI report images');
+        const Inputs_PDI_loading_images= document.getElementById('input-PDI loading images');
+        const reportImgs = Array.from(Input_PDI_report_images.files);
+        const loadingImgs = Array.from(Inputs_PDI_loading_images.files);
+
+        let reportImgs_c = await compressImages(reportImgs);
+        let loadingImgs_c = await compressImages(loadingImgs);
+
+        // Create multipart/form-data payload
+        const payload = new FormData();
+        payload.append('json', new Blob([JSON.stringify(data)], { type: 'application/json' }));
+        reportImgs_c.forEach((file,index)=>
+            payload.append('PDI_report_images', file, `PDI report ${index}.${file.name.split('.').pop()}`)
+        );
+        loadingImgs_c.forEach((file,index)=>
+            payload.append('PDI_loading_images', file, `Loading image ${index}.${file.name.split('.').pop()}`)
+        );
+
+        console.log(Array.from(payload.entries()));
+
+        showLoading(true);
         const response= await fetch(URL,{method:'POST',body:payload});
         console.log('Response:', response);
         if (response.ok){
@@ -127,8 +124,8 @@ async function submitForm(event){
         alert('Error submitting data');
     }
     finally{
-    disableForm(false);
-    showLoading(false);
+        disableForm(false);
+        showLoading(false);
     }
 }
 
